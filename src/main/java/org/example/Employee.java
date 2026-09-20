90d5266a34234a54cb8c7687e33a03981f8c1865
Denzel <kiandenzel@yahoo.com>
2026-09-20 09:57:31 -0400

    Part D done

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -8,7 +8,23 @@ class Employee {
     public Employee(double salary) {
         this.salary = salary;
     }
+
+    static void main(String[] args) {
+        Manager manager = new Manager(80000);
+
+        manager.giveRaise();
+
+        System.out.println(manager.salary);
+
+
+        manager.breakSalary();
+
+        System.out.println(manager.salary);
+    }
 }
+
+
+
 
 class Manager extends Employee {
 
@@ -34,4 +50,11 @@ class Manager extends Employee {
 
            Part C
          1. Employee will print first because without a parent class, child will not exist.
+
+           Part D
+         A. Salary should be protected so the child class can have access.
+         1. Yes, it can read & write.
+         2. Yes, in here breakSalary gives a negative value.
+         3. No, it's weaker, the parent class will be dependant to the child class.
+         4. Yes,  a private, a protected will risk to have a invalid state.
           */
